fb29ca4a903807f99891334c0c11cb0c0a8113fb
Raali <xdeadmonkx@gmail.com>
2020-06-13 14:43:50 +0300

    initial

---
+++ hello/hello/hello.cxx
@@ -0,0 +1,14 @@
+import module1;
+import module2;
+import module3;
+import module4;
+
+import std.core;
+
+int main (int argc, char* argv[])
+{
+	module1();
+	module2();
+	module3();
+	module4();
+}
